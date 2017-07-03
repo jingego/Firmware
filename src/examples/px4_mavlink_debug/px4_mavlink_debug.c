2e7a55ee6eac172f3e73238759cf6578371e4cf8
jingego <jingxuanjk@gmail.com>
2017-07-03 15:06:35 +0800

    send transition status back to ground station

--- src/examples/px4_mavlink_debug/px4_mavlink_debug.c
+++ src/examples/px4_mavlink_debug/px4_mavlink_debug.c
@@ -39,37 +39,77 @@
  */
 
 #include <px4_config.h>
+#include <px4_tasks.h>
+#include <px4_posix.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <poll.h>
-
+#include <string.h>
+#include <math.h>
 #include <systemlib/err.h>
-
 #include <uORB/uORB.h>
 #include <uORB/topics/debug_key_value.h>
+#include <uORB/topics/vtol_vehicle_status.h>
 
 __EXPORT int px4_mavlink_debug_main(int argc, char *argv[]);
 
 int px4_mavlink_debug_main(int argc, char *argv[])
 {
 	printf("Hello Debug!\n");
+    int trans_state_sub_fd=orb_subscribe(ORB_ID(vtol_vehicle_status));
+    orb_set_interval(trans_state_sub_fd, 500);
+    px4_pollfd_struct_t fds[] = {
+            { .fd = trans_state_sub_fd,   .events = POLLIN },
+          };
 
-	/* advertise debug value */
+    /* advertise debug value */
 	struct debug_key_value_s dbg = { .key = "velx", .value = 0.0f };
 	orb_advert_t pub_dbg = orb_advertise(ORB_ID(debug_key_value), &dbg);
 
-	int value_counter = 0;
-
-	while (value_counter < 100) {
+    //int value_counter = 0;
+    int error_counter = 0;
+    while (error_counter < 100) {
 		/* send one value */
-		dbg.value = value_counter;
-		orb_publish(ORB_ID(debug_key_value), pub_dbg, &dbg);
+        /* wait for sensor update of 1 file descriptor for 1000 ms (1 second) */
+        int poll_ret = px4_poll(fds, 1, 1000);
 
-		warnx("sent one more value..");
+        /* handle the poll result */
+        if (poll_ret == 0) {
+            /* this means none of our providers is giving us data */
+            PX4_ERR("Got no data within a second");
 
-		value_counter++;
+        } else if (poll_ret < 0) {
+            /* this is seriously bad - should be an emergency */
+            if (error_counter < 10 || error_counter % 50 == 0) {
+                /* use a counter to prevent flooding (and slowing us down) */
+                PX4_ERR("ERROR return value from poll(): %d", poll_ret);
+            }
+
+            error_counter++;
+
+        } else {
+
+            if (fds[0].revents & POLLIN) {
+                /* obtained data for the first file descriptor */
+                struct vtol_vehicle_status_s raw;
+                /* copy sensors raw data into local buffer */
+                orb_copy(ORB_ID(vtol_vehicle_status), trans_state_sub_fd, &raw);
+                PX4_INFO("Transition Status:\t%1f",
+                     (double)raw.vtol_in_rw_mode);
+
+                dbg.value = raw.vtol_in_rw_mode;
+                orb_publish(ORB_ID(debug_key_value), pub_dbg, &dbg);
+            }
+
+            /* there could be more file descriptors here, in the form like:
+             * if (fds[1..n].revents & POLLIN) {}
+             */
+        }
+        //warnx("sent one more value..");
+
+        //value_counter++;
 		usleep(500000);
 	}
-
+    PX4_INFO("exiting");
 	return 0;
 }
